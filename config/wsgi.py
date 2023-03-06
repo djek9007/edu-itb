f94e0b230820956ad5d33cae07b4a37acca889ec
djek9007 <djek9007@gmail.com>
2023-03-06 09:09:43 +0600

    wsgi

--- config/wsgi.py
+++ config/wsgi.py
@@ -16,4 +16,4 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root=os.getenv('ROOT'))
+application = WhiteNoise(application, root=os.getenv('WHITENOISEROOT'))
