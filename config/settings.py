22b5590611de9ec293129f5bedc9aeeb8ad09570
djek9007 <djek9007@gmail.com>
2023-03-06 11:19:38 +0600

    False debug

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = getenv('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'edu-itb.kz', 'http://edu-itb.kz', 'https://edu-itb.kz']
 
